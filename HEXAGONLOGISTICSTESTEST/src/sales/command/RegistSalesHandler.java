5efd3a243f97f82d4532830a57e5c8a040e74d23
hyunsik <hyunsik@DESKTOP-G3UKR3N>
2023-12-18 05:33:39 +0900

    백업

---
+++ HEXAGONLOGISTICSTESTEST/src/sales/command/RegistSalesHandler.java
@@ -0,0 +1,129 @@
+package sales.command;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mvc.command.CommandHandler;
+import sales.model.RegistRequest;
+import sales.service.RegistSalesService;
+
+public class RegistSalesHandler implements CommandHandler {
+
+	private static final String FORM_VIEW = "/WEB-INF/view/RegistSales.jsp";
+	private RegistSalesService salesService = new RegistSalesService();
+	
+	@Override
+	public String process(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		if (req.getMethod().equalsIgnoreCase("GET")) {
+			return processForm(req, res);
+		} else if (req.getMethod().equalsIgnoreCase("POST")) {
+			return processSubmit(req, res);
+		} else {
+			res.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+			return null;
+		}
+	}
+
+	private String processForm(HttpServletRequest req, HttpServletResponse res) {
+		return FORM_VIEW;
+	}
+	
+
+
+	private String processSubmit(HttpServletRequest req, HttpServletResponse res) {
+		
+		RegistRequest salesReq = new RegistRequest();
+		
+		Map<String, Boolean> errors = new HashMap<>();
+		req.setAttribute("errors", errors);
+		
+
+		int p_no = 0;
+		int s_seoul = 0;
+		int s_suwon = 0;
+		int s_incheon = 0;
+		
+		String p_noVal = req.getParameter("p_no");
+		String s_seoulVal = req.getParameter("s_seoul");
+		String s_suwonVal = req.getParameter("s_suwon");
+		String s_incheonVal = req.getParameter("s_incheon");
+		String s_dateVal = req.getParameter("s_date");
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date utilDate = null;
+		java.sql.Date s_date = null;
+		
+
+		
+	    // 판매량을 하나라도 입력하지 않았을 경우 
+	    if (s_seoulVal.trim().isEmpty() && s_suwonVal.trim().isEmpty() && s_incheonVal.trim().isEmpty()) {
+	        errors.put("salesError", true);
+	        return FORM_VIEW;
+	    }
+	    
+	    try {
+	    if (s_dateVal == null || s_dateVal.trim().isEmpty()) {
+	    	errors.put("salesDateError", true);
+	    	return FORM_VIEW;
+	    } else {
+	    	utilDate = dateFormat.parse(s_dateVal);
+	    	s_date = new java.sql.Date(utilDate.getTime());
+	    }
+	    } catch (ParseException e) {
+	    	e.printStackTrace();
+	    }
+		
+		try {
+		p_no = Integer.parseInt(p_noVal);
+		s_seoul = !s_seoulVal.isEmpty() ? Integer.parseInt(s_seoulVal) : 0;
+		s_suwon = !s_suwonVal.isEmpty() ? Integer.parseInt(s_suwonVal) : 0;
+		s_incheon = !s_incheonVal.isEmpty() ? Integer.parseInt(s_incheonVal) : 0;
+		
+
+		
+		salesReq.setP_no(p_no);
+		salesReq.setS_seoul(s_seoul);
+		salesReq.setS_suwon(s_suwon);
+		salesReq.setS_incheon(s_incheon);
+		salesReq.setS_date(s_date);
+
+		
+		System.out.println("p_no from request: " + p_noVal);
+		System.out.println("p_noVal: " + p_noVal);
+		System.out.println("s_seoulVal: " + s_seoulVal);
+		System.out.println("s_seoul: " + s_seoul);
+		System.out.println("s_suwonVal: " + s_suwonVal);
+		System.out.println("s_suwon: " + s_suwon);
+		System.out.println("s_incheonVal: " + s_incheonVal);
+		System.out.println("s_incheon: " + s_incheon);
+		System.out.println("s_dateVal: " + s_dateVal);
+		System.out.println("s_date: " + s_date);
+
+
+		salesReq.validate(errors);
+		
+		
+
+		if (!errors.isEmpty()) {
+			return FORM_VIEW;
+		}
+		
+
+		salesService.registSales(salesReq);
+		return FORM_VIEW;
+		} catch (NumberFormatException e) {
+			errors.put("numberFormat", true);
+			return FORM_VIEW;
+		} catch (Exception e) {
+			return FORM_VIEW;
+		}
+
+	}
+}
---
+++ HEXAGONLOGISTICSTESTEST/src/sales/dao/SalesDao.java
@@ -0,0 +1,138 @@
+
+package sales.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import jdbc.JdbcUtil;
+import product.model.Product;
+import sales.model.RegistRequest;
+import sales.model.Sales;
+import sales.model.SalesList;
+
+public class SalesDao {
+
+	public Product selectByP_no(Connection conn, String p_no) throws SQLException {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement("select * from product_list where p_no = ?");
+			pstmt.setString(1, p_no);
+			rs = pstmt.executeQuery();
+			Product product = null;
+			if (rs.next()) {
+				product = new Product(rs.getInt("p_no"), 
+						rs.getString("p_name"), 
+						rs.getInt("p_seoul"),
+						rs.getInt("p_suwon"), 
+						rs.getInt("p_incheon"), 
+						rs.getInt("price")
+						);
+
+			}
+			return product;
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+		}
+	}
+
+	public void insert(Connection conn, RegistRequest sal) throws SQLException {
+		try (PreparedStatement pstmt = conn
+				.prepareStatement("insert into sales_list values(S_SALES_LIST.NEXTVAL,?,?,?,?,?)")) {
+			pstmt.setInt(1, sal.getP_no());
+			pstmt.setInt(2, sal.getS_seoul());
+			pstmt.setInt(3, sal.getS_suwon());
+			pstmt.setInt(4, sal.getS_incheon());
+			pstmt.setDate(5, sal.getS_date());
+			pstmt.executeUpdate();
+		}
+	}
+
+	public void update(Connection conn, RegistRequest salesReq) throws SQLException {
+		try (PreparedStatement pstmt = conn.prepareStatement(
+				"update PRODUCT_LIST set P_SEOUL=P_SEOUL-?, P_SUWON=P_SUWON-?,P_INCHEON=P_INCHEON-? where S_NUM=S_SALES_LIST.CURRVAL")) {
+			pstmt.setInt(1, salesReq.getS_seoul());
+			pstmt.setInt(2, salesReq.getS_suwon());
+			pstmt.setInt(3, salesReq.getS_incheon());
+			pstmt.executeUpdate();
+		}
+
+	}
+	
+	public List<SalesList> select1(Connection conn, int startRow, int size) throws SQLException {
+		
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<SalesList> result = new ArrayList<>();
+		try {
+
+			pstmt = conn.prepareStatement("select s_num, sales_list.p_no, product_list.p_name, s_seoul, s_suwon, s_incheon, s_date from sales_list, product_list  where product_list.p_no = sales_list.p_no and s_num between ? and ? order by s_num");
+				  pstmt.setInt(1, startRow);
+				  pstmt.setInt(2, size);
+				  rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				result.add(convertSales(rs));
+			}
+			System.out.println("ProductDao3" + startRow);
+			System.out.println("ProductDao3" + size);
+			
+			return result;
+			
+			
+	
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(pstmt);
+
+		}
+	}
+
+	
+	
+	
+	
+	public int selectCount(Connection conn) throws SQLException {
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("select count(*) from product_list");
+
+			if (rs.next()) {
+				return rs.getInt(1);
+				
+
+			}
+			return 0;
+
+		} finally {
+			JdbcUtil.close(rs);
+			JdbcUtil.close(stmt);
+
+		}
+	}
+	
+	private SalesList convertSales(ResultSet rs) throws SQLException {
+		return new SalesList(rs.getInt("s_num"),
+				rs.getInt("p_no"),
+				rs.getString("p_name"),
+				rs.getInt("p_seoul"),
+				rs.getInt("p_suwon"),
+				rs.getInt("p_incheon"),
+				rs.getString("s_date")); 
+
+	}
+
+	
+	
+	
+	
+
+}
